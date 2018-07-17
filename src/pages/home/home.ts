5133100e1a556c5f48c286776ecfe461cce23951
saadsufyan <saad.sufyan@mobiwhiz.com>
2018-07-17 17:07:56 +0500

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public items :any = [
+    {
+      "id": 1,
+      "name": "product1",
+      "description": "des",
+      "image": "../../assets/imgs/tshirt.jpg"
+    },
+    {
+      "id": 2,
+      "name": "product2",
+      "description": "des",
+      "image": "../../assets/imgs/tshirt1.jpg"
+    },
+    {
+      "id": 3,
+      "name": "product3",
+      "description": "des",
+      "image": "../../assets/imgs/tshirt2.jpg"
+    },
+    {
+      "id": 4,
+      "name": "product4",
+      "description": "des",
+      "image": "../../assets/imgs/tshirt3.png"
+    },
+  ]
   constructor(public navCtrl: NavController) {
+
+  }
+
+  getData(){
 
   }
 
